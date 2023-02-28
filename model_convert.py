cabecd9eb9dec7480f116dc411b71ea11adf0c75
r-9li <2659320958@qq.com>
2023-02-28 09:52:45 +0800

    plan 2

---
+++ model_convert.py
@@ -0,0 +1,17 @@
+import trimesh
+import os
+
+p = {
+    # Folder containing the BOP datasets.
+    'dataset_path': '/media/r/T7 Shield/lm_1',
+}
+
+models_path = os.path.join(p['dataset_path'], 'models')
+for root, dirs, files in os.walk(models_path):
+    for file in files:
+        if file.endswith('ply'):
+            path = os.path.join(root, file)
+            print(path)
+            mesh = trimesh.load(path)
+            _ = mesh.apply_obb()
+            mesh.export(file_obj=path)
